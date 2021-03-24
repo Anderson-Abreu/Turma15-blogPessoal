88ffbad751c03c7ba9a0c47c3dcd36b40808d36e
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-03-24 13:39:54 -0300

    Cadastro e Login completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +9,23 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
+import { InicioComponent } from './inicio/inicio.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent,
     MenuComponent,
     RodapeComponent,
     CadastrarComponent,
-    EntrarComponent
+    EntrarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
